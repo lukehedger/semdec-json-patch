f61974e32e1fdac79de3fe876645b0501dc17018
lukehedger <lh4@pm.me>
2022-09-06 15:45:59 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# semdec-json-patch
---
+++ patch.js
@@ -0,0 +1,30 @@
+import { applyPatch } from "json-joy/es6/json-patch";
+import Ajv from "ajv";
+import toSchema from "./schema/to.json" assert { type: "json" };
+import patch from "./patch/from-to.patch.json" assert { type: "json" };
+
+const ajv = new Ajv();
+
+const notification = {
+  notificationId: "123",
+  externalId: "T100",
+  message: "hello",
+};
+
+// TODO: Can add use existing values??
+// e.g. { "op": "add", "path": "/metadata/externalId", "value": "$.externalId" }
+
+const { doc: event } = applyPatch(notification, patch, false);
+
+// TODO: Catch runtime errors e.g. NOT_FOUND, others?
+// TODO: Test runtime error catching
+
+console.log(event);
+
+// TODO: Move schema validation to unit test, assert on validate.errors
+
+const validate = ajv.compile(toSchema);
+
+const valid = validate(event);
+
+if (!valid) console.log(validate.errors);
