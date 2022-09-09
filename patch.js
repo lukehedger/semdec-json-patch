bba96a78d070ad9b7d805d2dfe58d426aa28beab
lukehedger <lh4@pm.me>
2022-09-09 15:54:13 +0100

    Add some todos

--- patch.js
+++ patch.js
@@ -5,13 +5,15 @@ import patch from "./patch/from-to.patch.json" assert { type: "json" };
 
 const ajv = new Ajv();
 
+// TODO: Match notification/event schema to EB->CE (see PN)
+
 const notification = {
   notificationId: "123",
   externalId: "T100",
   message: "hello",
 };
 
-// TODO: Can add use existing values??
+// TODO: Can `add` use existing values??
 // e.g. { "op": "add", "path": "/metadata/externalId", "value": "$.externalId" }
 
 const { doc: event } = applyPatch(notification, patch, false);
@@ -28,3 +30,7 @@ const validate = ajv.compile(toSchema);
 const valid = validate(event);
 
 if (!valid) console.log(validate.errors);
+
+// TODO: How could we use Schema Registry as a form of Contract Testing (e.g. message pacts)?
+
+// TODO: Add AsyncAPI and Backstage
