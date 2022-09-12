6eb92d8613cf44fb5cdb55a30de37e29772de22f
lukehedger <lh4@pm.me>
2022-09-12 12:17:17 +0100

    Use payment example

--- patch.js
+++ patch.js
@@ -1,36 +1,20 @@
 import { applyPatch } from "json-joy/es6/json-patch";
 import Ajv from "ajv";
-import toSchema from "./schema/to.json" assert { type: "json" };
-import patch from "./patch/from-to.patch.json" assert { type: "json" };
+
+import paymentCloudEvents from "./schema/payment.cloudevents.json" assert { type: "json" };
+import paymentPatch from "./patch/payment.patch.json" assert { type: "json" };
+import paymentNotification from "./data/payment-notification.json" assert { type: "json" };
 
 const ajv = new Ajv();
 
-// TODO: Match notification/event schema to EB->CE (see PN)
+const { doc: event } = applyPatch(paymentNotification, paymentPatch, false);
 
-const notification = {
-  notificationId: "123",
-  externalId: "T100",
-  message: "hello",
-};
-
-// TODO: Can `add` use existing values??
-// e.g. { "op": "add", "path": "/metadata/externalId", "value": "$.externalId" }
-
-const { doc: event } = applyPatch(notification, patch, false);
-
-// TODO: Catch runtime errors e.g. NOT_FOUND, others?
-// TODO: Test runtime error catching
-
-console.log(event);
-
-// TODO: Move schema validation to unit test, assert on validate.errors
-
-const validate = ajv.compile(toSchema);
+const validate = ajv.compile(paymentCloudEvents);
 
 const valid = validate(event);
 
-if (!valid) console.log(validate.errors);
-
-// TODO: How could we use Schema Registry as a form of Contract Testing (e.g. message pacts)?
-
-// TODO: Add AsyncAPI and Backstage
+if (!valid) {
+  console.error(validate.errors);
+} else {
+  console.log(event);
+}
